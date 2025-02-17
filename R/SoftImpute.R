6359c0d1b18efdec41af35474c02a8f387119b79
lavrad99 <57375795+lavrad99@users.noreply.github.com>
2025-02-17 17:34:30 +0000

    Create SoftImpute.R

---
+++ R/SoftImpute.R
@@ -0,0 +1,24 @@
+SoftImpute <- function(X_miss , debias = FALSE){
+  
+  # Function for SoftImpute
+  
+  data_SVD <- impute_mean(X_miss)
+  
+  singular_vals <- svd(data_SVD)$d
+  
+  dim_elbow <- elbow_rule(y = singular_vals)
+  
+  lambda_elbow <- singular_vals[dim_elbow]
+  
+  if(debias == F){
+    return(softImpute::complete(X_miss , softImpute(x = X_miss, rank.max = dim_elbow,
+                                                    lambda = lambda_elbow, maxit = 300)))
+  }else{
+    
+    return(softImpute::complete(X_miss ,
+                                deBias(X_miss , softImpute(x = X_miss,
+                                                           rank.max = dim_elbow,
+                                                           lambda = lambda_elbow, maxit = 300))))
+  }
+  
+}
